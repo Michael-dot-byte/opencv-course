5b4181f771feed66d19da6790405318c52b51c86
Michael-dot-byte <ploessner@hotmail.de>
2021-04-10 13:02:32 +0200

    rescale and read

--- Section Mike #1 - Basics/read.py
+++ Section Mike #1 - Basics/read.py
@@ -1,6 +1,34 @@
 import cv2 as cv
 
-img = cv.imread('../Resources/Photos/cat_large.jpg')
+#resizing and rescaling
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1]* scale)
+    height = int(frame.shape[0]*scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_LINEAR)
 
+#change resolution
+def changeResolution(width)
+
+#reading images
+'''
+img = cv.imread('../Resources/Photos/cat.jpg')
+img_resized = rescaleFrame(img, 0.4)
 cv.imshow('cat', img)
+cv.imshow('cat_resized', img_resized)
 cv.waitKey(0)
+'''
+
+#reading videos
+capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame, 0.2)
+    cv.imshow('Video', frame)
+    cv.imshow('Video resized', frame_resized)
+
+    #end video when pressing letter d
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
---
+++ Section Mike #1 - Basics/rescale.py
@@ -0,0 +1,11 @@
+import cv2 as cv2
+
+img = cv.imread('../Resource/Photos/cat_large.jpg')
+cv.imshow('Cat', img)
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1]* scale)
+    height = int(frame.shape[0]*scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_LINEAR)
+
